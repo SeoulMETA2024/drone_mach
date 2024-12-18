4664f1f957f4012ee4a6f30d1a78a72c27d5b778
ehdrjs13 <ehdrjs070102@gmail.com>
2024-12-18 15:37:06 +0900

    mod kalmanfilter applyment

--- control/core/hovering.py
+++ control/core/hovering.py
@@ -3,9 +3,10 @@ from pid import PID
 from sensor import Sensor, KalmanFilter
 import sensor
 
+#조립 후 기재하기
 BUS_NUM = 
 SERIAL_NUM = 
-class Hover(PID):
+class Hover(PID,KarmanFilter):
     def __init__(self,motor_1,motor_2,motor_3,motor_4,Kp,Ki,Kd):
         self.sensor = Sensor(BUS_NUM, SERIAL_NUM)
 
@@ -13,6 +14,10 @@ class Hover(PID):
         self.motor_2 = Motor(motor_2)
         self.motor_3 = Motor(motor_3)
         self.motor_4 = Motor(motor_4)
+
+        self.KalmanFilter_x = KalmanFilter()
+        self.KalmanFilter_y = KalmanFilter()
+        self.KalmanFilter_z = KalmanFilter()
 
         self.angle = 0
 
@@ -27,9 +32,9 @@ class Hover(PID):
 
         data = self.sensor.measure()
 
-        gyro_x = data[0]
-        gyro_y = data[1]
-        gyro_z = data[2]
+        gyro_x = self.KalmanFilter_x.update_estimate(data[0])
+        gyro_y = self.KalmanFilter_y.update_estimate(data[1])
+        gyro_z = self.KalmanFilter_z.update_estimate(data[2])
 
         target_angle = target_angle
         
